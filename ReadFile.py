e7b1789e781ef945ea7e305caeddbc70584c3831
RuneyR <gamerthe11@gmail.com>
2021-10-28 10:48:44 -0700

    Program now reads from a text file. Text file will be generated if not found at launch.

--- ReadFile.py
+++ ReadFile.py
@@ -1,13 +1,40 @@
 import os
-
-# Program script will create 2 files. One for consumer key and secret. The other for creating generated tokens.
-# Program will also read from Consumer file for OAUTH.
 import sys
 
 FILE_TO_ACCESS = 'Keys.txt'
 TEXT_TO_WRITE = "Replace this line with consumer key!\nReplace this line with consumer secret!\nReplace this line " \
                 "with access key!\nReplace this line with consumer secret!"
 global fileEmpty
+
+
+# If the file exists, do nothing. If it doesn't catch the error resulting from no file found and create it.
+def check_key_file():
+    global fileEmpty
+    try:
+        fileEmpty = os.stat(FILE_TO_ACCESS).st_size == 0
+    except OSError:
+        print("File not found.Creating txt.Write down keys within the file. File location=program was run.\n")
+        consumerFileWrite = open(FILE_TO_ACCESS, "w")
+        consumerFileWrite.close()
+        consumerFileWrite = open(FILE_TO_ACCESS, "a")
+        consumerFileWrite.write(TEXT_TO_WRITE)
+        consumerFileWrite.close()
+        sys.exit()
+
+
+def get_keys():
+    cfr = open(FILE_TO_ACCESS, "r")
+    ck = cfr.read().split("\n")
+    cfr.close()
+    return ck
+import os
+import sys
+
+FILE_TO_ACCESS = 'Keys.txt'
+TEXT_TO_WRITE = "Replace this line with consumer key!\nReplace this line with consumer secret!\nReplace this line " \
+                "with access key!\nReplace this line with consumer secret!"
+global fileEmpty
+
 
 # If the file exists, do nothing. If it doesn't catch the error resulting from no file found and create it.
 def check_key_file():
